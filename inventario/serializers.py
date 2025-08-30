bd64c751f15898b736361dce8fd1f4f20ccc52bd
Martin Castillo <58961096+waskull@users.noreply.github.com>
2025-08-30 16:15:28 -0400

    clase 6 cruds
    post api view

---
+++ inventario/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import Product, Category
+import uuid
+def formatDate():
+    return serializers.DateTimeField(format="%Y/%m/%d %H:%M")
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+    created_at = formatDate()
+    updated_at = formatDate()
+    
+class CategoryPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('name', 'desc')
+
+class ProductSerializer(serializers.ModelSerializer):
+    cat = CategorySerializer(many=False, read_only=True)
+    class Meta:
+        model = Product
+        fields = ('id','created_at', 'updated_at', 'uuid', 'cat','name')
+
+    created_at = formatDate()
+    updated_at = formatDate()
+    uuid = serializers.SerializerMethodField()
+
+    def get_uuid(self, objj):
+        return uuid.uuid4()
+    
+def formatDate():
+    return serializers.DateTimeField(format="%Y/%m/%d %H:%M")
+
+class ProductPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('id','price','name', 'qty', 'cat')
--- inventario/views.py
+++ inventario/views.py
@@ -1,30 +1,34 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework import status
 from rest_framework.response import Response
 from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer, ProductPostSerializer, CategoryPostSerializer
 # Create your views here.
 
 class ProductList(APIView):
     def get(self, request):
         products = Product.objects.all()
-        data = [
-            {
-                "id": p.id,
-                "name": p.name,
-                "quantity": p.qty,
-                "category": p.cat.name
-            } for p in products
-        ]
-        return Response(data)
+        name = request.query_params.get('name', None)
+        if name:
+            products = products.filter(name__icontains=name)
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    def post(self, request):
+        serializer = ProductPostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class CategoryList(APIView):
     def get(self, request):
         cats = Category.objects.all()
-        data = [
-            {
-                "id": c.id,
-                "name": c.name,
-                "description": c.desc
-            } for c in cats
-        ]
-        return Response(data)
+        serializer = CategorySerializer(cats, many=True)
+        return Response(serializer.data)
+    def post(self, request):
+        serializer = CategoryPostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
